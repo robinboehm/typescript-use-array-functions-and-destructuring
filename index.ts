66eb3a709266003224fc6f0770f47d52a9238a00
robinboehm <robin.boehm@workshops.de>
2020-11-06 06:49:42 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
 // Import stylesheets
 import './style.css';
+import {getConnectionData, getUsers} from './helper';
 
 
-// Task: Define an interface for User and define a generic Array. Then find all the typos that happend here.
-const users = [
-  {firstname: 'Robin', lastname:'Böhm', Admin: true},
-  {firstname: 'Lisa', lastname:'Carls', admin: true},
-  {firstname: 'Anne', lastname:'Garagiola', admin: true},
-  {firstName: 'Max', lastName:'Löffler', admin: true},
-  {fristname: 'Peter', lastname:'Umme', admin: true},
-  {firstname: 'Jessica', lastname:'Jones🦸‍♀️️', damin: true},
-  {firstname: 'Olga', lastname:'Rasslith'},
-  {first_name: 'Phin', last_name:'Li', admin: true},
+// Task 1: Use Destructuring to make code more readable
+const conData = getConnectionData();
+const hostname = conData[0];
+const port = conData[1];
+const protocol = conData[2];
 
-]
+console.log(`${protocol}://${hostname}:${port}`);
+
+
+// Task 2: Use Array.map instead of a loop
+const users = getUsers();
+const userNames = [];
+console.log(users);
+for(let i=0; i<users.length;i++){
+  userNames.push(users[i].name);
+}
+
+console.log(userNames);
 
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>Use arrays functions and destructuring`;
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>Use arrays functions and destructuring`; 
